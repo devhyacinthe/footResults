5fae039c88bc10c5d83d72216edc84d6ad6cf60c
devhyacinthe <yacinthedalmeida2002@gmail.com>
2022-03-15 23:31:41 +0000

    fin de la page home

--- footresults/src/components/Card.js
+++ footresults/src/components/Card.js
@@ -4,11 +4,19 @@ import '../components/_card.css'
 const Card = ({ fixtures }) => {
     return (
         <div className="card">
-            <p className="event_status">{fixtures.event_status}</p>
-            <p>
-                <span>{fixtures.event_time}</span>
-                <span></span>
+            <p id="event_status">
+                {fixtures.event_status}
+                <p id="heure">{fixtures.event_time}</p>
             </p>
+
+            <div id="match"><img src={fixtures.home_team_logo} alt="" height="45px" /><span id="mat">{fixtures.event_home_team}</span></div>
+            <div id="as"><span>{fixtures.event_final_result}</span></div>
+            <div id="matcha"><span>{fixtures.event_away_team}</span><img src={fixtures.away_team_logo} alt="" height="45px" /></div>
+            <div id="stade">{fixtures.event_stadium}</div>
+            <div><img src={fixtures.league_logo} alt="" height="55px" /></div>
+
+
+
         </div>
     );
 };
